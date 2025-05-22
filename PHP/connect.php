187f557cb1df2d660bc60ece58d2b43cc1b13a90
Víctor <142942997+viictor0906@users.noreply.github.com>
2025-05-21 21:28:47 -0300

    Initial database integration in PHP
    Add connect function where the variables stores MySQL values to make connection checks.
    Add error handling to check connection with database.
    Add disconnect function, even if the integration is PDO, the disconnect function will be used to manually stop.

--- PHP/connect.php
+++ PHP/connect.php
@@ -1,30 +1,28 @@
 <?php
-function connectDB(){
+function connect(){
 	//Creating variables.
-	$servername="localhost";
-	$username="userTest";
-	$password="";
-	$dbName="gameHost";
-	
-	//Creating connection with database.
-	$conn=new mysqli($servername, $username, $password);
+	$dsn="mysql:host=localhost;dbname=hospedagem_jogos;charset=utf8mb4"; //Connection string, stores database informations to easily.
+	$username='userTest';
+	$password='1234';
 
-	//Checking connection state.
-	if($conn->connect_error){
-		die("Connection failed: " . $conn->connect_error); //"->" work for acess other variable value.
+	//Try connection with database.
+	try{
+		//Creating a new PDO instance.
+		$pdo=new PDO($dsn,$username,$password,[
+			PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, //Launch exceptions in error case.
+			PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC, //When we use SELECT, for example, the data return how ['column'=>value].
+			PDO::ATTR_EMULATE_PREPARES=>false //Use real prepares of MySql, more secure, avoid sqlInjection.
+		]);
+
+		return $pdo;
+
+	//If 'try' fail, kill connection and show the error.
+	} catch(PDOException $errorMessage){ //PDOException it's a error class, this argument catch the error, it's like true/false.
+		die("Conection error with database: " . $errorMessage->getMessage());
 	}
 
-	if($conn->(query)$sql===TRUE){
-		echo "Connection successfully";	
-	}else{
-		echo"Error creating database: " . $conn->error;
-	}
-
-	return $conn;
-}
-
-//Closing database connection.
-function closeCon($conn){
-	$conn->close();
+//Closing database connection. PDO close connection when the php code block reaches the end, but for manual closed, this function is here.
+function disconnect(){
+	$pdo=null;
 }
 ?>
