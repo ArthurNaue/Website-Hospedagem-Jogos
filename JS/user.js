5e4c0b7a7f5939ed9af54e7504b436713eb2e9c0
Michel_D_Souza <108071593+MysticDango@users.noreply.github.com>
2025-06-12 21:14:20 -0300

    Mais atualizações do user_info

--- JS/user.js
+++ JS/user.js
@@ -1,11 +1,35 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+document.addEventListener('DOMContentLoaded', function(){
+    const imgDiv = document.querySelector('.user');
+    const img = document.querySelector('#photo');
+    const file = document.querySelector('#file');
+    const uploadBtn = document.querySelector('#uploadBtn');
+
+    if (!imgDiv || !img || !file || !uploadBtn){
+        console.error("Ai naaaao");
+        return;
+    }
+
+imgDiv.addEventListener('mouseenter', function(){
+    uploadBtn.style.display = "block";
+});
+
+imgDiv.addEventListener('mouseleave', function(){
+    uploadBtn.style.display = "none";
+});
+
+file.addEventListener('change', function(){
+
+    const choosedFile = this.files[0];
+
+    if (choosedFile) {
+
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(){
+            img.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(choosedFile);
+        }
+    });
+});
