fd968671b27fbe78039f4a511533ae9534b46752
Víctor <142942997+viictor0906@users.noreply.github.com>
2025-06-25 18:57:58 -0300

    Update PHP Structure

--- index.php
+++ index.php
@@ -27,23 +27,71 @@ $row=$stmt->fetch();
     <a href="https://github.com/ArthurNaue/Website-Hospedagem-Jogos">GITHUB</a>
     </div> 
 <div id="main">
-    <button id="button" class="openbtn" onclick="openOrCloseNav()">ABRIR</button>
-    <div>
-      <form action="php/user-register.php" method="post">
-        <label for="username">Name:</label>
-        <input type="text" id="username" name="username" required>
-        
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
-        
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required>
+  <button id="button" class="openbtn" onclick="openOrCloseNav()">ABRIR</button>
 
-        <button type="submit" name="user_register">Send</button>
-      </form>
-      <a href="php/user-delete.php?user_id=2" onclick="return confirm('Do you really want delete this user?')">Delete</a>
-    </div>
+  <h3>Register User:</h3>
+
+  <div>
+    <form action="php/user-register.php" method="post" enctype="multipart/form-data">
+      <label for="username">Name:</label>
+      <input type="text" id="username" name="username" required>
+      
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+      
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+
+      <label for="user_img">User img: </label>
+      <input type="file" id="user_img" name="user_image" required>
+
+      <button type="submit" name="user_register">Send</button>
+    </form>
+  </div>
+
+  
+  <ul></ul>
+
+  <div>
+    <form action="php/user-delete.php" method="post">
+      <label for="user_id">User ID to delete:</label>
+      <input type="text" id="user_id" name="user_id" required>
+
+      <button type="submit" onclick="return confirm('Are you sure you want to delete this user?')">Delete</button>
+    </form>
+  </div>
+
+  <h3>Show Users:</h3>
+
+  <div>
+    <form action="php/user-show.php" method="post" enctype="multipart/form-data">
+      <button type="submit">Show users:</button>
+    </form>
+  </div>
+
+  <h3>Update Users:</h3>
+
+  <div>
+    <form action="php/user-update.php" method="post" enctype="multipart/form-data">
+      <label for="user_id">User ID:</label>
+      <input type="text" id="user_id" name="user_id" required>
+
+      <label for="username">Username:</label>
+      <input type="text" id="username" name="username" required>
+
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required>
+
+      <label for="password">Password:</label>
+      <input type="password" id="password" name="password" required>
+
+      <label for="user_img">Profile Image:</label>
+      <input type="file" id="user_img" name="user_image" required>
+
+      <button type="submit" name="user_update" onclick="return confirm('Are you sure you want to update this user?')">Update</button>
+    </form>
+  </div>
+
 </div>
-
-    </body>
+</body>
 </html>
--- php/user-delete.php
+++ php/user-delete.php
@@ -2,10 +2,14 @@
 include 'connect.php';
 $pdo=connect_db();
 
-if(isset($_GET['user_id'])){
-    $id=intval($_GET['user_id']);
+if(isset($_POST['user_id'])){
+    $id=intval($_POST['user_id']);
     $sql_user_delete=$pdo->prepare('DELETE FROM users WHERE user_id=?');
-    $sql_user_delete->execute([$id]);
+    if($sql_user_delete->execute([$id])){
+        echo "User with ID $id deleted successfully.";
+    } else {
+        echo "Error deleting user.";
+    }
 } else{
     echo "User id don't informed!";
 }
